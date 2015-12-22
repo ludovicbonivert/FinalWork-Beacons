51e3da89350d22d0915b16d37a3c97b5b04b6ac6
Ludovic Bonivert <ludovic.bonivert@gmail.com>
2015-12-22 16:57:12 +0100

    Estimote tutorial - part 1 : setting up

---
+++ estimote-tutorial/Estimote-tutorial/Estimote-tutorial/ObjCBridge.h
@@ -0,0 +1,15 @@
+//
+//  ObjCBridge.h
+//  Estimote-tutorial
+//
+//  Created by Ludovic Bonivert on 22/12/15.
+//  Copyright © 2015 Ludovic Bonivert. All rights reserved.
+//
+
+#ifndef ObjCBridge_h
+#define ObjCBridge_h
+
+#import <EstimoteSDK/EstimoteSDK.h>
+
+
+#endif /* ObjCBridge_h */
---
+++ estimote-tutorial/Estimote-tutorial/Estimote-tutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Estimote-tutorial
+//
+//  Created by Ludovic Bonivert on 22/12/15.
+//  Copyright © 2015 Ludovic Bonivert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
